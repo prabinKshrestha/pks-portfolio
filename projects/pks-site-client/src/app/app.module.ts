7dd3a16aa291e844c38e2ec65764d8c9b63a55bf
prabinKshrestha <prabin2026@gmail.com>
2022-04-03 15:34:36 +0545

    Experience complete

--- projects/pks-site-client/src/app/app.module.ts
+++ projects/pks-site-client/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/abouts/about.component';
+import { EducationComponent } from './components/educations/education.component';
+import { ExperienceComponent } from './components/experiences/experience.component';
 import { SidenavComponent } from './components/sidenavs/sidenav.component';
+import { SkillComponent } from './components/skills/skill.component';
 import { WelcomeComponent } from './components/welcomes/welcome.component';
 
 @NgModule({
@@ -12,7 +15,10 @@ import { WelcomeComponent } from './components/welcomes/welcome.component';
         AppComponent,
         SidenavComponent,
         WelcomeComponent,
-        AboutComponent
+        AboutComponent,
+        EducationComponent,
+        SkillComponent,
+        ExperienceComponent,
     ],
     imports: [
         BrowserModule,
