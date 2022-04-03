8d217ddfee4e025f7b64ef63de8d60d9095a67a9
prabinKshrestha <prabin2026@gmail.com>
2022-04-03 10:33:25 +0545

    icons added in side nav

--- projects/pks-site-client/src/app/app.module.ts
+++ projects/pks-site-client/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './components/about/about.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AboutComponent } from './components/abouts/about.component';
+import { SidenavComponent } from './components/sidenavs/sidenav.component';
+import { WelcomeComponent } from './components/welcomes/welcome.component';
 
 @NgModule({
     declarations: [
---
+++ projects/pks-site-client/src/app/components/skills/skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pks-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss']
+})
+export class SkillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
