6478db54d07b568e758ce80ca2bab4126d421d96
prabinKshrestha <prabin2026@gmail.com>
2022-04-10 12:38:52 +0545

    responsive complete

--- projects/pks-site-client/src/app/app.component.ts
+++ projects/pks-site-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pks-root',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public isSideNavOpen: boolean = false;
+
+  constructor(public route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment: string | null) => {
+      this.isSideNavOpen = false;
+    });
+  }
+
+  public toggleSidenav(value: boolean = false) {
+    if (value) {
+      this.isSideNavOpen = false;
+    } else {
+      this.isSideNavOpen = !this.isSideNavOpen;
+    }
+  }
 }
